aa015f06e5b94517888d23c2333cad87cfb73754
yExplorer <xianheyu@gmail.com>
2017-02-27 21:54:08 -0500

    gary

---
+++ src/components/inputBlock.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class inputBlock extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {   
+        return (
+            <p> asdf asd</p>
+        );
+    }
+}
+
+export default inputBlock;
---
+++ style/mainStyle.js
@@ -0,0 +1,11 @@
+const mainStyle = {
+    pStep : {
+        'marginBottom' : 0
+    },
+    hStyle :{
+        'marginTop' : 0,
+        'marginBottom' : '0.2em'
+    }
+}
+
+export default mainStyle;
